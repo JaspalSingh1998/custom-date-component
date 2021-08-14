764e5ebbff1baf61a5ab551fba2e0abfb63eb725
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-14 11:31:34 +0530

    Made next and prev month button working

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-import { format, fromUnixTime, getUnixTime } from "date-fns";
+import {
+  addMonths,
+  format,
+  fromUnixTime,
+  getUnixTime,
+  subMonths,
+} from "date-fns";
 
 const datePickerButton = document.querySelector(".date-picker-button");
 const datePicker = document.querySelector(".date-picker");
 const datePickerHeaderText = document.querySelector(".current-month");
+const previousMonthButton = document.querySelector(".prev-month-button");
+const nextMonthButton = document.querySelector(".next-month-button");
+let currentDate = new Date();
 
 datePickerButton.addEventListener("click", (e) => {
   datePicker.classList.toggle("show");
   const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+  currentDate = selectedDate;
   setupDatePicker(selectedDate);
 });
 
@@ -15,8 +25,17 @@ function setDate(date) {
   datePickerButton.dataset.selectedDate = getUnixTime(date);
 }
 
-function setupDatePicker(selectedDate) {
-  datePickerHeaderText.textContent = format(selectedDate, "MMMM - y");
+function setupDatePicker() {
+  datePickerHeaderText.textContent = format(currentDate, "MMMM - y");
 }
+
+nextMonthButton.addEventListener("click", () => {
+  currentDate = addMonths(currentDate, 1);
+  setupDatePicker();
+});
+previousMonthButton.addEventListener("click", () => {
+  currentDate = subMonths(currentDate, 1);
+  setupDatePicker();
+});
 
 setDate(new Date());
